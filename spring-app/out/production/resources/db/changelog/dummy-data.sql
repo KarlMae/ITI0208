10f3bbee1cecac772ce2ad93cc0dfec385bd97ca
Rando <raluik@taltech.ee>
2019-02-27 17:02:34 +0200

    dummy data

---
+++ spring-app/out/production/resources/db/changelog/dummy-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO workouts (name, description) VALUES ("Planche basics", "The most popular calisthenics move");
+
+INSERT INTO workouts (name, description) VALUES ("Killer workout 5000", "Väga ull workout");
+
+INSERT INTO exercises (name, category) VALUES ("Tuck planche", "Compound exercise");
+
+INSERT INTO exercises (name, category) VALUES ("Burpee", "Compound exercise");
---
+++ spring-app/src/main/resources/db/changelog/dummy-data.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+  training.workouts(name, description)
+VALUES
+  ('Planche basics', 'The most popular calisthenics move');
+
+INSERT INTO
+  training.workouts(name, description)
+VALUES
+  ('Killer workout 5000', 'Väga ull workout');
+
+INSERT INTO
+  training.exercises(name, category)
+VALUES
+  ('Tuck planche', 'Compound exercise');
+
+INSERT INTO
+  training.exercises(name, category)
+VALUES
+  ('Burpee', 'Compound exercise');
