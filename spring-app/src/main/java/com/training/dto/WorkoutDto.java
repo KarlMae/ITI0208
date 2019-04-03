4cb40ae80b9ed7c182a0baa232deb92671e872a4
KarlMae <karl@mae.ee>
2019-04-03 17:49:10 +0300

    Minor changes

--- spring-app/src/main/java/com/training/dto/WorkoutDto.java
+++ spring-app/src/main/java/com/training/dto/WorkoutDto.java
@@ -1,19 +1,17 @@
 package com.training.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 public class WorkoutDto {
 
     private int id;
     private String name;
     private String description;
-    private List<ExerciseGroupDto> exerciseGroups;
+    private List<ExerciseGroupDto> exerciseGroups = new ArrayList<>();
     private List<ExerciseDto> exercises;
 
     public void addExerciseGroup(ExerciseGroupDto group) {
